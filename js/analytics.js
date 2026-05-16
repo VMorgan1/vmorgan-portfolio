738bb974ec860abf52e0716a71b853ddb8def9ec
VMorgan1 <victorejakhaegbe607@gmail.com>
2026-05-16 04:09:47 +0100

    Add Google Analytics tracking

---
+++ js/analytics.js
@@ -0,0 +1,9 @@
+const GA_MEASUREMENT_ID = "G-H7GJYF94VT";
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+
+gtag("js", new Date());
+gtag("config", GA_MEASUREMENT_ID);
